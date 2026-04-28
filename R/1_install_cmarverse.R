619eea0b513553119594eac72d34dadac8c4362b
Danielle Dempsey <ddempsey@perennia.ca>
2026-04-28 18:54:26 -0300

    initial commit

---
+++ R/1_install_cmarverse.R
@@ -0,0 +1,30 @@
+# April 28, 2026
+
+# run this code to install the CMAR R packages that you will be using
+# to work with the CMP data
+
+# these packages are not on CRAN, so you will install them from GitHub
+
+# you only need to run this script once. If any of the packages are updated,
+# just re-run the code to install that package for the most recent version
+
+library(devtools) # to install packages from GitHub
+
+
+# sensorstrings -----------------------------------------------------------
+
+install_github("Centre-for-Marine-Applied-Research/sensorstrings")
+
+# qaqcmar -----------------------------------------------------------
+
+install_github("Centre-for-Marine-Applied-Research/qaqcmar")
+
+# cmpr -----------------------------------------------------------
+
+install_github("Centre-for-Marine-Applied-Research/cmpr")
+
+
+
+
+
+

a6b7cccbb1bdb153e9d97a6e07410c5a5774de0c
Danielle Dempsey <ddempsey@perennia.ca>
2026-06-08 15:42:24 -0300

    calculate wind speed and direction averages using vectors
    streamline reading in data

--- R/1.0_format_climate_data.R
+++ R/1.0_format_climate_data.R
@@ -1,3 +1,4 @@
+# June 8, 2026
 # May 19, 2026
 
 # hourly meteorlogical data from https://climate.weather.gc.ca/index_e.html
@@ -8,6 +9,7 @@
 # From website:
 # binned into 2-hour intervals
 # took average of 2-hours intervals
+# use vector averages for wind speed and direction
 
 library(data.table)
 library(dplyr)
@@ -16,39 +18,44 @@ library(lubridate)
 library(purrr)
 library(tidyr)
 
+source(here("functions/helpers.R"))
 
 round_int <- "2 hours"
 
 # import data -------------------------------------------------------------
 
-# beaver island
-beav <- map_df(
-  .x = list.files(here("data-raw/beaver_island_(aut)"), full.names = TRUE), 
-  .f = fread, fill = TRUE
-) 
+dat_folders <- list_climate_data_folders()
 
-# hart_island_(aut) 
-hart <- map_df(
-  .x = list.files(here("data-raw/hart_island_(aut)"), full.names = TRUE), 
-  .f = fread, fill = TRUE
-) 
+dat <- map_df(.x = dat_folders, .f = foo)
 
-# lunenburg met data 
-lune <- map_df(
-  .x = list.files(here("data-raw/lunenburg"), full.names = TRUE), 
-  .f = fread, fill = TRUE
-) 
-
-# yarmouth_rcs
-yar <- map_df(
-  .x = list.files(here("data-raw/yarmouth_rcs"), full.names = TRUE), 
-  .f = fread, fill = TRUE
-) 
+# # beaver island
+# beav <- map_df(
+#   .x = list.files(here("data-raw/beaver_island_(aut)"), full.names = TRUE), 
+#   .f = fread, fill = TRUE
+# ) 
+# 
+# # hart_island_(aut) 
+# hart <- map_df(
+#   .x = list.files(here("data-raw/hart_island_(aut)"), full.names = TRUE), 
+#   .f = fread, fill = TRUE
+# ) 
+# 
+# # lunenburg met data 
+# lune <- map_df(
+#   .x = list.files(here("data-raw/lunenburg"), full.names = TRUE), 
+#   .f = fread, fill = TRUE
+# ) 
+# 
+# # yarmouth_rcs
+# yar <- map_df(
+#   .x = list.files(here("data-raw/yarmouth_rcs"), full.names = TRUE), 
+#   .f = fread, fill = TRUE
+# ) 
 
 
 # format ------------------------------------------------------------------
 
-met_dat <- bind_rows(beav, hart, lune, yar) %>% 
+met_dat <- dat %>% 
   select(
     longitude = `Longitude (x)`,
     latitude = `Latitude (y)`,
@@ -71,27 +78,65 @@ met_dat <- bind_rows(beav, hart, lune, yar) %>%
     timestamp_utc = if_else(
       nchar(timestamp_utc) == 16, paste0(timestamp_utc, ":00"), timestamp_utc),
     timestamp_utc = as_datetime(timestamp_utc)
-  ) 
-
-# average -----------------------------------------------------------------
-
-met_dat_avg <- met_dat %>% 
+  ) %>% 
   pivot_longer(
     cols = air_temperature:wind_chill, values_to = "value", names_to = "variable"
   ) %>% 
   filter(!is.na(value)) %>% 
   mutate(
     round_timestamp_utc = round_date(timestamp_utc, unit = round_int)
+  ) 
+
+# average -----------------------------------------------------------------
+
+scalar_avg <- met_dat %>% 
+  filter(
+    !variable %in% c("wind_speed_km_per_hour", "wind_direction_10_degree")
   ) %>% 
-  summarise(
+ summarise(
     value = mean(value), 
     n = n(),
     .by = c(station, variable, round_timestamp_utc)
+  ) 
+
+# wind speed & direction
+vec_avg <- met_dat %>% 
+  filter(
+    variable %in% c("wind_speed_km_per_hour", "wind_direction_10_degree")
+  ) %>%
+  pivot_wider(names_from = "variable", values_from = "value") %>% 
+  mutate(
+    wind_direction_degree = wind_direction_10_degree * 10,
+    
+    v_x = wind_speed_km_per_hour * sin(wind_direction_degree * pi / 180),
+    v_y = wind_speed_km_per_hour * cos(wind_direction_degree * pi / 180)
   ) %>% 
-  pivot_wider(
-    names_from = "variable", values_from = "value"
+  summarise(
+    v_x_mean = mean(v_x),
+    v_y_mean = mean(v_y), 
+    n = n(),
+    .by = c(station, round_timestamp_utc)
   ) %>% 
-  rename(timestamp_utc = round_timestamp_utc)
+  mutate(
+    wind_direction_degree = round(
+      atan2(v_x_mean, v_y_mean) * 180/pi, digits = 2
+    ),
+    # maps from (-180 to 180) to (0, 359)
+    wind_direction_degree = (360 + wind_direction_degree) %% 360,
+    
+    wind_speed_km_per_hour = round(sqrt(v_y_mean^2 + v_x_mean^2), digits = 2)
+  ) %>% 
+  pivot_longer(
+    cols = c("wind_speed_km_per_hour", "wind_direction_degree"),
+    values_to = "value", names_to = "variable"
+  ) 
+
+
+met_dat_avg <- scalar_avg %>% 
+  bind_rows(vec_avg) %>% 
+  rename(timestamp_utc = round_timestamp_utc) %>% 
+  select(-c(v_x_mean, v_y_mean)) %>% 
+  arrange(station, timestamp_utc)
 
 
 # station locations -------------------------------------------------------
